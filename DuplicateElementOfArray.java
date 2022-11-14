6a84b5f9b02af250eaa1682e44775e9b3d7b8489
Shital <114717834+ShitalKarad@users.noreply.github.com>
2022-11-14 10:47:19 -0800

    Update DuplicateElementOfArray.java

--- DuplicateElementOfArray.java
+++ DuplicateElementOfArray.java
@@ -1,15 +1,22 @@
 package com.day3_Array_Handling;
 
 public class DuplicateElementOfArray {
-    public static void main(String[] args)
-{
-    System.out.print("Duplicates are ");
-    int[] dup={ 11,21,22,22,11,13,15,1,1,3,3};
-    for(int i=0; i<dup.length; i++){
-        for(int j=i+1 ;j<dup.length; j++) {
-            if (dup[i] == dup[j])
-                System.out.println(dup[j]+" ");
+    public static void main(String[] args){
+            System.out.println("Duplicate numbers are ");
+            int[] arr ={11,12,8,3,3,2,5,7,8,8,7,5,8,8,11,11,3,3};
+
+            for(int i = 0;i<arr.length;i++){
+                int temp=0;
+                for(int j=i+1;j<arr.length;j++){
+                    if(arr[i]==arr[j])
+                        temp++;
+                }
+                if (temp==1){
+                  System.out.println(arr[i]);
+
+
+                }
+            }
         }
     }
-}
 }
